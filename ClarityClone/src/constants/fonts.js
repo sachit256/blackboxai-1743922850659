97927684480671d326430ce86a20233765350009
sachit256 <140400666+sachit256@users.noreply.github.com>
2025-04-06 12:31:26 +0530

    Add ClarityClone/src/constants/fonts.js 1743922886303

---
+++ ClarityClone/src/constants/fonts.js
@@ -0,0 +1,33 @@
+export const FONTS = {
+  sizes: {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 20,
+    xxl: 24,
+    xxxl: 32,
+  },
+  weights: {
+    light: '300',
+    regular: '400',
+    medium: '500',
+    semibold: '600',
+    bold: '700',
+  },
+  families: {
+    // Using system fonts initially, can be updated when custom fonts are added
+    regular: 'System',
+    medium: 'System',
+    bold: 'System',
+  },
+};
+
+export const SPACING = {
+  xs: 4,
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 32,
+  xxl: 40,
+};
